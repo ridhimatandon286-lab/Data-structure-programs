082303a6b3d8528ebb451182b909c95a2958d9aa
ridhimatandon286-lab <ridhimatandon286@gmail.com>
2025-09-15 18:23:06 +0530

    Add files via upload

--- factorial.c
+++ factorial.c
@@ -1,21 +1,22 @@
+//wap to factorial of number
 #include<stdio.h>
-int fact(int n)
+int fact( int n)
 {
-      if(n>1)
-      {
-      	return n*fact(n-1);
-    
-	  }
-	  else
-	  {
-	  	return 1;
-	  }
+	if(n>1)
+	{
+		return n*fact(n-1);
+	}
+	else
+	{
+		return n;
+	}
 }
-int main()
+int main( )
 {
 	int n;
 	printf("Enter a number:");
 	scanf("%d",&n);
 	printf("Factorial=%d",fact(n));
-	return n;
+	
+	
 }
