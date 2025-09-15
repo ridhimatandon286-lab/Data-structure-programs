24b19bf3d47c8cd41b25a8761366711a6a2d301d
ridhimatandon286-lab <ridhimatandon286@gmail.com>
2025-09-15 10:53:16 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int fact(int n)
+{
+      if(n>1)
+      {
+      	return n*fact(n-1);
+    
+	  }
+	  else
+	  {
+	  	return 1;
+	  }
+}
+int main()
+{
+	int n;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	printf("Factorial=%d",fact(n));
+	return n;
+}
