cb4d20134421a9b8d59edf3fa7607f7234cebe4a
ridhimatandon286-lab <ridhimatandon286@gmail.com>
2025-09-22 11:19:23 +0530

    Add files via upload

---
+++ Array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	int A[5]={1,2,3,4,5};
+	for(i=0;i<5;i++)
+	{
+		printf("%d",A[i]);
+	}
+	return 0;
+}
+
+
+
+																																																								
+
